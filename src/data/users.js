364a911e8c16a5aada890868842324f53a36cd37
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-05-01 07:40:29 -0300

    Create users.js

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        name: 'Natanael Vieira',
+        email: 'n.vieira@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2023/08/25/21/44/ai-generated-8213763_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Thais Vieira',
+        email: 't.vieira@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2020/08/02/18/08/woman-5458225_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Abigail Vieira',
+        email: 'a.vieira@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/04/01/11/29/avatar-1300370_1280.png'
+    },
+    {
+        id: 4,
+        name: 'Rute Vieira',
+        email: 'r.vieira@empmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/03/31/21/40/angry-1296580_1280.png'
+    },
+]
