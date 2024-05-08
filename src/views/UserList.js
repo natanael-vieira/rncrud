f12a2235227feddd1a5fe3fad63ca5dbae19e429
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-05-08 06:24:14 -0300

    Update UserList.js

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -5,14 +5,17 @@ import UsersContext from '../context/UsersContext'
 
 export default props => {
 
-    const { state } = useContext(UsersContext)
+    const { state, dispatch } = useContext(UsersContext)
 
     function confirmUserDeletion(user) {
         Alert.alert('Excluir Usuário', 'Deseja excluir o Usuário?', [
             {
                 text: 'Sim',
                 onPress() {
-                    console.warn('delete ' + user.id)
+                    dispatch({
+                        type: 'deleteUser',
+                        payload: user
+                    })
                 }
             },
             {
