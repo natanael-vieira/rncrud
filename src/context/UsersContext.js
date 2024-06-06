2ce500dbb73f3489af9d2e8d861793d0ddb594b5
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-06-06 06:31:35 -0300

    Update UsersContext.js

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -1,21 +1,42 @@
+
 import React, { createContext, useReducer } from 'react'
 import users from '../data/users'
 
 const initialState = { users }
 const UsersContext = createContext({})
 
-export const UsersProvider  = props => {
+const actions = {
+    createUser(state, action) {
+        const user = action.payload
+        user.id = Math.random()
+        return {
+            ...state,
+            users: [...state.users, user],
+        }
+    },
+    updateUser(state, action) {
+        const updated = action.payload
+        return {
+            ...state,
+            users: state.users.map(u => u.id === updated.id ? updated : u)
+        }
+    },
+    deleteUser(state, action) {
+        const user = action.payload
+        return {
+            ...state,
+            users: state.users.filter(u => u.id !== user.id)
+        }
+    }
+}
+
+export const UsersProvider = props => {
 
     function reducer(state, action) {
-        if(action.type === 'deleteUser') {
-            const user = action.payload
-            return {
-                //...state,
-                user: state.users.filter(u => u.id !== user.id)
-            }
-        }
-        return state
+        const fn = actions[action.type]
+        return fn ? fn(state, action) : state
     }
+
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
