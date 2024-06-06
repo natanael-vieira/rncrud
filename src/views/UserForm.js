f25cdcde2a3d35a374767a4d28e2b3ec3ab14df0
Natanael Vieira <89619697+natanael-vieira@users.noreply.github.com>
2024-06-06 06:31:37 -0300

    Update UserForm.js

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import UsersContext from '../context/UsersContext'
 
 export default ({route, navigation}) => {
-    const [user, setUser ] = useState(route.params ? route.params: {})
+    const [ user, setUser ] = useState(route.params ? route.params: {})
+    const { dispatch } = useContext(UsersContext)
+    
     return (
         <View style={style.form}>
             <Text>Nome</Text>
